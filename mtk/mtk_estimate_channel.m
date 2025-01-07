4a37419057c7da06ecf183d3e9ef22c29003a74b
Luiz Sampaio <sampaio.ime@gmail.com>
2025-01-07 20:56:59 +0000

    mtk_estimate_channel: blmmse and additive quantizer noise are the same
    As we created the mtk_prepare_channel_estimation function, which
    pre-evaluates important variables, blmmse and additive quantizer noise
    become the same algorithm (with different C_r_real, of course).

--- mtk/mtk_estimate_channel.m
+++ mtk/mtk_estimate_channel.m
@@ -10,30 +10,7 @@ function H_hat = mtk_estimate_channel(method, params)
             R = mtk_util_quantize(Y, 1/sqrt(2));
 
             H_hat = (1/sqrt(rho))*(R*conj(Phi))/(conj(Phi')*conj(Phi));
-        case 'blmmse'
-            C_h_real = params.C_h_real;
-            H = params.H;
-            K = params.K;
-            M = params.M;
-            N = params.N;
-            Phi = params.Phi;
-            rho = params.rho;
-
-            Phi_tilde_real = params.Phi_tilde_real;
-            C_r_real = params.C_r_real;
-
-            Y = sqrt(rho)*H*conj(Phi') + N;
-            
-            y = Y(:);
-            y_real = [real(y); imag(y)];
-
-            r = mtk_util_quantize(y_real, 1);
-            
-            h_hat_real = C_h_real * (Phi_tilde_real' / C_r_real) * r;
-            h_hat = h_hat_real(1 : M*K) + 1i * h_hat_real(M*K+1 : 2*M*K);
-
-            H_hat = reshape(h_hat, [M, K]);
-        case 'additive-quantizer-noise'
+        case {'blmmse','additive-quantizer-noise'}
             C_h_real = params.C_h_real;
             H = params.H;
             K = params.K;
