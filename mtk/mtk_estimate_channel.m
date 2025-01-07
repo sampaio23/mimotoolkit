64c3af9918bb69b740e00354301b990e9bfda264
Luiz Sampaio <sampaio.ime@gmail.com>
2025-01-07 21:55:49 +0000

    mtk_estimate_channel: adding blmmse-cn for comparator networks

--- mtk/mtk_estimate_channel.m
+++ mtk/mtk_estimate_channel.m
@@ -33,6 +33,30 @@ function H_hat = mtk_estimate_channel(method, params)
             h_hat = h_hat_real(1 : M*K) + 1i * h_hat_real(M*K+1 : 2*M*K);
 
             H_hat = reshape(h_hat, [M, K]);
+        case 'blmmse-cn'
+            Beff = params.Beff;
+            C_h_real = params.C_h_real;
+            H = params.H;
+            K = params.K;
+            M = params.M;
+            N = params.N;
+            Phi = params.Phi;
+            rho = params.rho;
+
+            Phi_tilde_real = params.Phi_tilde_real;
+            C_r_real = params.C_r_real;
+
+            Y = sqrt(rho)*H*conj(Phi') + N;
+
+            y = Y(:);
+            y_real = [real(y); imag(y)];
+
+            r = mtk_util_quantize(Beff * y_real, 1);
+
+            h_hat_real = C_h_real * (Phi_tilde_real' / C_r_real) * r;
+            h_hat = h_hat_real(1 : M*K) + 1i * h_hat_real(M*K+1 : 2*M*K);
+
+            H_hat = reshape(h_hat, [M, K]);
         otherwise
             error('Channel type not implemented');
     end
--- mtk/mtk_prepare_channel_estimation.m
+++ mtk/mtk_prepare_channel_estimation.m
@@ -33,6 +33,25 @@ function params = mtk_prepare_channel_estimation(method, params)
 
             C_y_r = Phi_line_real * C_h_real * Phi_line_real' + 1/2 * eye(2*M*tau);
             params.C_r_real = (1-2/pi)*eye(2*tau*M) + A_real*C_y_r*A_real';
+        case 'blmmse-cn'
+            alpha = params.alpha;
+            Beff = params.Beff;
+            C_h_real = params.C_h_real;
+            K = params.K;
+            M = params.M;
+            Phi = params.Phi;
+            rho = params.rho;
+            tau = params.tau;
+
+            Phi_line = kron(Phi, sqrt(rho)*eye(M));
+            Phi_line_real = [ real(Phi_line) -imag(Phi_line); imag(Phi_line) real(Phi_line)];
+            A_real = sqrt(2/pi) * sqrt(2/(K*rho + 1)) * eye((2*M + alpha) * tau);
+            params.Phi_tilde_real = A_real * Beff * Phi_line_real;
+
+            C_y_r = Beff * (Phi_line_real * C_h_real * Phi_line_real' + 1/2 * eye(2*M*tau)) * Beff';
+            Sigma_y_real = diag(diag(C_y_r));
+            invsqr_Sigma_y_real = sqrt(inv(Sigma_y_real));
+            params.C_r_real = real(2/pi * asin(invsqr_Sigma_y_real * real(C_y_r) * invsqr_Sigma_y_real));
         otherwise
             error('Channel type not implemented');
     end
