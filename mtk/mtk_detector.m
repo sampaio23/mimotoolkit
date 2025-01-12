5e3d9ddc9678fb0e6ef57f5e9e5ed2795f72864f
Luiz Sampaio <sampaio.ime@gmail.com>
2025-01-12 22:10:44 +0000

    mtk_detector: fixing bug in calculating asin function of complex number

--- mtk/mtk_detector.m
+++ mtk/mtk_detector.m
@@ -21,7 +21,7 @@ function G = mtk_detector(detector, params)
             S_r = (H_hat * H_hat') + 1/params.rho*eye(params.M);
             S_y = sqrt(inv(diag(diag(S_r))));
             A = sqrt(2/pi) * S_y * H_hat;
-            G = A'/(2/pi*(asin(S_y * S_r * S_y)));
+            G = A'/(2/pi*(asin(S_y * real(S_r) * S_y) + 1i*asin(S_y * imag(S_r) * S_y)));
         otherwise
             error('Detector not implemented');
     end
