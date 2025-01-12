2533fdbdc18146379f4af955c0814c060b231ed7
Luiz Sampaio <sampaio.ime@gmail.com>
2025-01-12 20:49:06 +0000

    mtk_detector: adding BZF detector

--- mtk/mtk_detector.m
+++ mtk/mtk_detector.m
@@ -4,19 +4,24 @@ function G = mtk_detector(detector, params)
         case 'zf'        
             G = pinv(H_hat);
         case {'mf', 'mrc'}
-            G = inv(diag(diag(H_hat' * H_hat))) * H_hat';
+            G = (diag(diag(H_hat' * H_hat))) \ H_hat';
         case 'mmse'
             G = (1/params.rho*eye(params.K) + H_hat'*H_hat)\H_hat';
+        case 'bzf'
+            S_r = (H_hat * H_hat') + 1/params.rho*eye(params.M);
+            S_y = sqrt(inv(diag(diag(S_r))));
+            A = sqrt(2/pi) * S_y * H_hat;
+            G = pinv(A);
         case 'bmrc'
             S_r = (H_hat * H_hat') + 1/params.rho*eye(params.M);
             S_y = sqrt(inv(diag(diag(S_r))));
             A = sqrt(2/pi) * S_y * H_hat;
-            G = inv(diag(diag(A' * A))) * A';
-        case 'blmmse'
+            G = (diag(diag(A' * A))) \ A';
+        case 'bmmse'
             S_r = (H_hat * H_hat') + 1/params.rho*eye(params.M);
             S_y = sqrt(inv(diag(diag(S_r))));
             A = sqrt(2/pi) * S_y * H_hat;
-            G = A'/(A*A' + 2/pi*(asin(S_y * S_r * S_y) - S_y * S_r * S_y + 1/params.rho * inv(diag(diag(S_r)))));
+            G = A'/(2/pi*(asin(S_y * S_r * S_y)));
         otherwise
             error('Detector not implemented');
     end
